9eda2a4e03320202885f8432bede969d72ab2af9
jserra99 <progamer5051@gmail.com>
2023-11-09 21:14:30 -0800

    done

--- row_puzzle.py
+++ row_puzzle.py
@@ -3,16 +3,25 @@
 # Date: 11/2/2023
 # Description: Project-6d; Row Puzzle
 
-def row_puzzle(row: list[int], index=0):
-    """ Returns true if the puzzle is solvable. """
+
+def row_puzzle(row: list[int], index=0, memo=None):
+    """
+    Returns true if the puzzle is solvable.
+    """
+    if memo is None:
+        memo = {}
+    if index in memo:
+        return memo[index]
     if index == len(row) - 1:
+        memo[index] = True
         return True
-    if index >= len(row) or row[index] == 0:
+    if index < 0 or index >= len(row) or row[index] == 0:
+        memo[index] = False
         return False
     shift = row[index]
     row[index] = 0
-    if row_puzzle(row, index + shift) or row_puzzle(row, index - shift):
-        row[index] = shift
-        return True
+    right = index + shift < len(row) and row_puzzle(row, index + shift, memo)
+    left = index - shift >= 0 and row_puzzle(row, index - shift, memo)
     row[index] = shift
-    return False
+    memo[index] = right or left
+    return memo[index]
